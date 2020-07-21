b2ffdd99230f09ff80d1eaa25d9caea158095a9a
Hugo Rodrigez <hugor37@gmail.com>
2020-07-21 21:29:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
-first_number = 8# Add your variables here
-second_number = 9
+first_number = 9# Add your variables here
+second_number = 8
 sum = first_number ++ second_number
 difference = first_number -- second_number
